c5bd1d9fc2e2f2f6c8d7950d0e4549b2878bcd33
brahim <brahim.carrilho@gmail.com>
2023-12-15 00:21:13 +0100

    add diagonal moves

--- ChessPiece.cpp
+++ ChessPiece.cpp
@@ -31,9 +31,33 @@ void addVerticalAndHorizontalMoves(std::vector<std::pair<int, int>> &moves, Ches
     }
 }
 
+void addDiagonalMoves(std::vector<std::pair<int, int>> &moves, ChessPiece* p, int r, int k, Game &g) {
+    for (int n = 0; n < 4; n++ ) {
+        int dirR = n < 2 ? 1 : -1;
+        int dirK = n % 2 == 0 ? 1 : -1;
+
+        for (int i = 1; i < 8; i++) {
+            int newR = r + i * dirR;
+            int newK = k + i * dirK;
+
+            if (newR < 0 || newR > 7 || newK < 0 || newK > 7) break;
+
+            ChessPiece* piece = g.getPiece(newR, newK);
+            if (piece == nullptr) {
+                moves.emplace_back(newR, newK);
+            } else {
+                if (piece->getKleur() != p->getKleur()) {
+                    moves.emplace_back(newR, newK);
+                }
+                break;
+            }
+        }
+    }
+}
+
 std::vector<std::pair<int, int>> Rook::getMoves(int r, int k, Game &g) {
     std::vector<std::pair<int, int>> moves;
-    addVerticalAndHorizontalMoves(this, r, k, g);
+    addVerticalAndHorizontalMoves(moves, this, r, k, g);
     return moves;
 }
 
@@ -44,11 +68,13 @@ std::vector<std::pair<int, int>> Knight::getMoves(int r, int k, Game &g) {
 std::vector<std::pair<int, int>> Bishop::getMoves(int r, int k, Game &g) {
     std::vector<std::pair<int, int>> moves;
     addDiagonalMoves(moves, this, r, k, g);
+    return moves;
 }
 
 std::vector<std::pair<int, int>> Queen::getMoves(int r, int k, Game &g) {
     std::vector<std::pair<int, int>> moves;
     addVerticalAndHorizontalMoves(moves, this, r, k, g);
+    addDiagonalMoves(moves, this, r, k, g);
     return moves;
 }
 

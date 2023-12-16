944ba771813ab9114ce6f86eb64f9bfe28a2a71e
brahim <brahim.carrilho@gmail.com>
2023-12-16 03:16:54 +0100

    i can't be bothered

--- ChessPiece.cpp
+++ ChessPiece.cpp
@@ -11,7 +11,7 @@ bool isInBounds(int r, int k) {
     return r >= 0 && r < 8 && k >= 0 && k < 8;
 }
 
-void addOrthogonalMoves(std::vector<std::pair<int, int>> &moves, ChessPiece* p, int r, int k, const Game &g, bool captureOnly = false) {
+void addOrthogonalMoves(std::vector<std::pair<int, int>> &moves, ChessPiece* p, int r, int k, const Game &g) {
     for (int n = 0; n < 4; n++ ) {
         int start = n < 2 ? r : k;
         int dir = n % 2 == 0 ? 1 : -1;
@@ -24,9 +24,9 @@ void addOrthogonalMoves(std::vector<std::pair<int, int>> &moves, ChessPiece* p, 
 
             ChessPiece* piece = g.getPiece(newR, newK);
 
-            if (piece == nullptr && !captureOnly) {
+            if (piece == nullptr) {
                 moves.emplace_back(newR, newK);
-            } else if (piece != nullptr) {
+            } else {
                 if (piece->getColor() != p->getColor()) {
                     moves.emplace_back(newR, newK);
                 }
@@ -36,7 +36,7 @@ void addOrthogonalMoves(std::vector<std::pair<int, int>> &moves, ChessPiece* p, 
     }
 }
 
-void addDiagonalMoves(std::vector<std::pair<int, int>> &moves, ChessPiece* p, int r, int k, const Game &g, bool captureOnly = false) {
+void addDiagonalMoves(std::vector<std::pair<int, int>> &moves, ChessPiece* p, int r, int k, const Game &g) {
     for (int n = 0; n < 4; n++ ) {
         int dirR = n < 2 ? 1 : -1;
         int dirK = n % 2 == 0 ? 1 : -1;
@@ -48,64 +48,14 @@ void addDiagonalMoves(std::vector<std::pair<int, int>> &moves, ChessPiece* p, in
             if (!isInBounds(newR, newK)) break;
 
             ChessPiece* piece = g.getPiece(newR, newK);
-            if (piece == nullptr && !captureOnly) {
+            if (piece == nullptr){
                 moves.emplace_back(newR, newK);
-            } else if (piece != nullptr) {
+            } else {
                 if (piece->getColor() != p->getColor()) {
                     moves.emplace_back(newR, newK);
                 }
                 break;
             }
-        }
-    }
-}
-
-void removeDiscoveredCheckMoves(std::vector<std::pair<int, int>> &moves, ChessPiece* p, int r, int k, Game &g) {
-    auto kingPos = g.getPosition(Piece::King, p->getColor());
-
-    std::vector<std::pair<int, int>> discoveredCheckPieceOrth;
-    std::vector<std::pair<int, int>> discoveredCheckPieceDia;
-
-    g.setPiece(r, k, nullptr);
-    addOrthogonalMoves(discoveredCheckPieceOrth, p, kingPos.first, kingPos.second, g, true);
-    addDiagonalMoves(discoveredCheckPieceDia, p, kingPos.first, kingPos.second, g, true);
-    g.setPiece(r, k , p);
-
-    for (auto it = discoveredCheckPieceOrth.begin(); it != discoveredCheckPieceOrth.end();) {
-        auto pt = g.getPiece(it->first, it->second)->piece().type();
-        if (pt != Piece::Queen && pt != Piece::Rook) {
-            it = discoveredCheckPieceOrth.erase(it);
-        } else it++;
-    }
-    for (auto it = discoveredCheckPieceDia.begin(); it != discoveredCheckPieceDia.end();) {
-        auto piece = g.getPiece(it->first, it->second)->piece().type();
-        if (piece != Piece::Queen && piece != Piece::Bishop) {
-            it = discoveredCheckPieceDia.erase(it);
-        } else it++;
-    }
-
-    size_t amount = discoveredCheckPieceOrth.size() + discoveredCheckPieceDia.size();
-    if (amount > 1) { // King is in check by 2 pieces
-        moves.clear();
-        return;
-    }
-
-    // Remove moves that don't block the discovered check piece
-    if (discoveredCheckPieceDia.size() == 1) {
-        auto threatPos = discoveredCheckPieceDia[0];
-        for (auto it = moves.begin(); it != moves.end();) {
-            if (threatPos.first - it->first != threatPos.second - it->second || // Move is not on the same diagonal
-                ((threatPos.first < kingPos.first) != (it->first < kingPos.first) || (threatPos.second < kingPos.second) != (it->second < kingPos.second) ) ) { // Move is not in the same direction
-                it = moves.erase(it);
-            } else it++;
-        }
-    } else if (discoveredCheckPieceOrth.size() == 1) {
-        auto threatPos = discoveredCheckPieceOrth[0];
-        for (auto it = moves.begin(); it != moves.end(); ) {
-            if ((threatPos.first == kingPos.first) != (it->first == kingPos.first) || (threatPos.second == kingPos.second) != (it->second == kingPos.second) // Move is not on the same orthogonal
-                || ((threatPos.first < kingPos.first) != (it->first < kingPos.first) || (threatPos.second < kingPos.second) != (it->second < kingPos.second) )) {  // Move is not in the same direction
-                it = moves.erase(it);
-            } else it++;
         }
     }
 }
@@ -198,25 +148,9 @@ std::vector<std::pair<int, int>> Pawn::getMoves(int r, int k, const Game &g) {
 }
 
 std::vector<std::pair<int, int>> King::getAllowedMoves(int r, int k, Game &g) {
-    std::vector<std::pair<int, int>> moves = getMoves(r, k, g);
-
-    auto it = moves.begin();
-    while (it < moves.end()) {
-        int newR = it->first;
-        int newK = it->second;
-        auto replaced = g.getPiece(newR, newK);
-        g.setPiece(newR, newK, this);
-        g.setPiece(r, k, nullptr);
-        if (g.inCheck(getColor())) {
-            it = moves.erase(it);
-        } else it++;
-
-        g.setPiece(newR, newK, replaced);
-        g.setPiece(r, k, this);
-    }
+    std::vector<std::pair<int, int>> moves = ChessPiece::getAllowedMoves(r, k, g);
 
     if (hasMoved) return moves;
-
     for (int n = 0; n < 2; n++){
         Rook* rook = dynamic_cast<Rook*>(g.getPiece(r, n ? 0 : 7));
         if (rook == nullptr) continue;
@@ -240,7 +174,21 @@ std::vector<std::pair<int, int>> King::getAllowedMoves(int r, int k, Game &g) {
 
 std::vector<std::pair<int, int>> ChessPiece::getAllowedMoves(int r, int k, Game &g) {
     std::vector<std::pair<int, int>> moves = getMoves(r, k, g);
-    removeDiscoveredCheckMoves(moves, this, r, k, g);
+
+    auto it = moves.begin();
+    while (it < moves.end()) {
+        int newR = it->first;
+        int newK = it->second;
+        auto replaced = g.getPiece(newR, newK);
+        g.setPiece(newR, newK, this);
+        g.setPiece(r, k, nullptr);
+        if (g.inCheck(getColor())) {
+            it = moves.erase(it);
+        } else it++;
+
+        g.setPiece(newR, newK, replaced);
+        g.setPiece(r, k, this);
+    }
     return moves;
 }
 
